7f4e80bce090b8f8563132deae0c030c092df50d
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2025-07-03 12:07:43 +0530

    dark:scroll

--- src/main.tsx
+++ src/main.tsx
@@ -11,14 +11,14 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <div className="think-blue-scrollbar h-screen w-screen overflow-hidden dark:bg-gray-900">
+        <div className=" h-screen w-screen overflow-hidden dark:bg-gray-900">
           <div className="fixed top-0 left-14 right-0 h-14 bg-[#1579EB] z-10">
             <TopNav title="Mifos Product Demo" />
           </div>
           <div className="fixed top-0 left-0 w-14 h-screen bg-white dark:bg-gray-900 z-10">
             <SideNav />
           </div>
-          <div className="ml-14 mt-14 h-[calc(100vh-3.5rem)] overflow-auto flex flex-col">
+          <div className="think-blue-scrollbar ml-14 mt-14 h-[calc(100vh-3.5rem)] overflow-auto flex flex-col">
             <div className="flex-grow">
               <App />
             </div>
