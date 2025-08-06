25c9ea98d77fcfc5b000b5003a7f9d82c9f30753
Pranav Deshmukh <113667044+pranav-deshmukh@users.noreply.github.com>
2025-08-06 19:26:47 +0530

    fix: reload of iframe on dpg tab change (#57)

--- src/lib/demofileparser/getBaseUrl.ts
+++ src/lib/demofileparser/getBaseUrl.ts
@@ -5,16 +5,16 @@ export const mapUrl = new Map<string, string>([
 
 export const getUniqueBaseUrls = (
   url: string,
-  setBaseUrls: React.Dispatch<React.SetStateAction<string[]>>
+  setBaseUrls: React.Dispatch<React.SetStateAction<Map<string, string>>>
 ) => {
   const parsedUrl = new URL(url);
   const baseUrl = `${parsedUrl.protocol}//${parsedUrl.hostname}`;
   if (mapUrl.has(baseUrl)) {
     setBaseUrls(prev => {
-      if (prev.includes(baseUrl)) {
-        return prev;
-      }
-      return [...prev, baseUrl];
+      const newMap = new Map(prev);
+      newMap.set(baseUrl, url);
+      return newMap;
     });
   }
+  return baseUrl;
 };
