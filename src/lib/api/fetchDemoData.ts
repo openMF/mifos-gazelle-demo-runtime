7b0bb46e098ec93b39a6ee06821e463ad2343de2
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2025-08-27 11:58:40 +0530

    feat: infinite scroll horizontal; fix: slug creation error for spaced titles

--- src/lib/api/fetchDemoData.ts
+++ src/lib/api/fetchDemoData.ts
@@ -4,5 +4,6 @@ export const fetchDemoData = async (demotitle: string) => {
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/demoData`, {
     params: { demotitle },
   });
+  console.log(response);
   return response.data;
 };
